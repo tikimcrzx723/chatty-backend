44515e36256c0aa2b780e0245b33cb7f685e2b50
tikimcrzx723 <tikimcrzx723@gmail.com>
2022-12-30 18:52:35 -0800

    init auth feature module and add Joi Validator

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Application } from 'express';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default (app: Application) => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   const routes = () => {};
   routes();
 };
--- src/setupServer.ts
+++ src/setupServer.ts
@@ -11,9 +11,9 @@ import { createClient } from 'redis';
 import { createAdapter } from '@socket.io/redis-adapter';
 import Logger from 'bunyan';
 import 'express-async-errors';
-import { config } from './config';
-import applicationRoutes from './routes';
-import { IErrorResponse, CustomError } from './shared/globals/helpers/error-handler';
+import { config } from '@root/config';
+import applicationRoutes from '@root/routes';
+import { CustomError, IErrorResponse } from '@global/helpers/error-handler';
 
 const SERVER_PORT = 5000;
 const log: Logger = config.createLogger('server');
@@ -37,7 +37,7 @@ export class ChattyServer {
     app.use(
       cookierSession({
         name: 'session',
-        keys: [config.SECRET_KEY_ONE!, config.SECRET_KEY_TWO!],
+        keys: [`${config.SECRET_KEY_ONE}`, `${config.SECRET_KEY_TWO}`],
         maxAge: 24 * 7 * 3600000,
         secure: config.NODE_ENV !== 'development'
       })
